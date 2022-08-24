8ba31c08299704f82990c0151cbfbdf403fc6bdf
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-24 14:54:11 -0400

    added firebase functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
     return (
         <>
             <Main />
-            <Row title='Upcoming' fetchURL={requests.requestUpcoming} />
-            <Row title='Popular' fetchURL={requests.requestPopular} />
-            <Row title='Top Rated' fetchURL={requests.requestTopRated} />
-            <Row title='Trending' fetchURL={requests.requestTrending} />
+            <Row rowID='1' title='Upcoming' fetchURL={requests.requestUpcoming} />
+            <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} />
+            <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated} />
+            <Row rowID='4' title='Trending' fetchURL={requests.requestTrending} />
         </>
     );
 };
