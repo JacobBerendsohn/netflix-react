8f81be2114b9ea4d211504f2e472f4b01b32e418
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-25 12:31:04 -0400

    Added firebase functions for user authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
